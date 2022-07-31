3f1c7afce0878a661297cdb7963e64fa9c482627
drp227 <drp22786@hotmail.com>
2022-07-31 10:17:17 -0500

    setting up the prompts

--- script.js
+++ script.js
@@ -12,3 +12,28 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()"
+  window.alert("Please follow the steps to generate your new password.");
+  var passwordLengthPrompt = 
+    window.prompt("Please choose a character length of your password. Must be between 8-128 characters long."
+    );
+  var lowercasePrompt = 
+    window.prompt("Would you like lowercase letters in your password? Click OK for YES or click Cancel for NO"
+    );
+  var uppercasePrompt = 
+    window.prompt("Would you like uppercase letters in your password? Click OK for YES or click Cancel for NO"
+    );
+  var numbersPrompt = 
+    window.prompt("Would you like numbers in your password? Click OK for YES or click Cancel for NO"
+    );
+  var specialPrompt =
+    window.prompt("Would you like special characters in your password? Click OK for YES or click Cancel for NO"
+    );
+}
+
+// function to set characters
+var passwordLengthPrompt = function() {
+  var length
+}
