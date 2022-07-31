f8848599cc3867b1b820e1896bd7832c71fd34d3
drp227 <drp22786@hotmail.com>
2022-07-31 11:03:07 -0500

    changed some prompts to confirms

--- script.js
+++ script.js
@@ -17,23 +17,26 @@ function generatePassword() {
   var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()"
   window.alert("Please follow the steps to generate your new password.");
   var passwordLengthPrompt = 
-    window.prompt("Please choose a character length of your password. Must be between 8-128 characters long."
+    parseInt(window.prompt("Please choose a character length of your password. Must be between 8-128 characters long."
+    ));
+      if (passwordLengthPrompt < 8 || passwordLengthPrompt > 128) {
+        window.alert("You need to choose between 8-128 characters!");
+        return generatePassword();
+      } else if (!passwordLengthPrompt) {
+        window.alert("You need to choose between 8-128 characters!");
+        return generatePassword();
+      }
+  var lowercaseConfirm = 
+    window.confirm("Would you like lowercase letters in your password? Click OK for YES or click Cancel for NO"
     );
-  var lowercasePrompt = 
-    window.prompt("Would you like lowercase letters in your password? Click OK for YES or click Cancel for NO"
+  var uppercaseConfirm = 
+    window.confirm("Would you like uppercase letters in your password? Click OK for YES or click Cancel for NO"
     );
-  var uppercasePrompt = 
-    window.prompt("Would you like uppercase letters in your password? Click OK for YES or click Cancel for NO"
+  var numbersConfirm = 
+    window.confirm("Would you like numbers in your password? Click OK for YES or click Cancel for NO"
     );
-  var numbersPrompt = 
-    window.prompt("Would you like numbers in your password? Click OK for YES or click Cancel for NO"
-    );
-  var specialPrompt =
-    window.prompt("Would you like special characters in your password? Click OK for YES or click Cancel for NO"
+  var specialConfirm =
+    window.confirm("Would you like special characters in your password? Click OK for YES or click Cancel for NO"
     );
 }
 
-// function to set characters
-var passwordLengthPrompt = function() {
-  var length
-}
